4f3d6e7ef928e9c02d3bc08eb7f01f312aa8d74c
haikaljs <haikaljavascript@gmail.com>
2023-08-14 20:44:47 +0800

    Login, logout, error message, admin guard setup

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -12,12 +12,35 @@ use Auth;
 class AdminLoginController extends Controller
 {
     public function index(){
-        $pass = Hash::make('1234');
-        dd($pass);
+        // $pass = Hash::make('123');
+        // dd($pass);
         return view('admin.login');
     }
 
     public function forget_password(){
         return view('admin.forget_password');
+    }
+
+    public function login_submit(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]); 
+
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if(Auth::guard('admin')->attempt($credential)){
+            return redirect()->route('admin_home');
+        }else{
+            return redirect()->route('admin_login')->with('error', 'Information is incorrect!');
+        }
+    }
+
+    public function logout(){
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin_login');
     }
 }
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -25,14 +25,31 @@
                         <div class="card card-primary border-box">
                             <div class="card-header card-header-auth">
                                 <h4 class="text-center">Admin Panel Login</h4>
+                               
                             </div>
                             <div class="card-body card-body-auth">
-                                <form method="POST" action="index.html">
+                                @if(session()->get('error'))
+                                <div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+                                    <strong>{{ session()->get('error') }}</strong> 
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                    </button>
+                                  </div>
+                                 @endif
+                                <form method="POST" action="{{ route('admin_login_submit') }}">
+                                    @csrf
                                     <div class="form-group">
-                                        <input type="email" class="form-control" name="email" placeholder="Email Address" value="" autofocus>
+                                        <input type="text" class="form-control 
+                                        @error('email') is-invalid @enderror" name="email" placeholder="Email Address" value="{{ old('email') }}" autofocus>
+                                        @error('email')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" name="password"  placeholder="Password">
+                                        <input type="password" class="form-control  @error('password') is-invalid @enderror" name="password"  placeholder="Password">
+                                        @error('password')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                     </div>
                                     <div class="form-group">
                                         <button type="submit" class="btn btn-primary btn-lg btn-block">
