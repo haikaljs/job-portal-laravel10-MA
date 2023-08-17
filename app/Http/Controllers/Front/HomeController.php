24610fd87d013e376aa4e01f799341bc21ccf258
haikaljs <haikaljavascript@gmail.com>
2023-08-17 11:03:31 +0800

    Display jobCategory to front

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PageHomeItem;
+use App\Models\JobCategory;
 
 class HomeController extends Controller
 {
     public function index(){
         $home_page_data = PageHomeItem::where('id', 1)->first();
-        return view('front.home', compact('home_page_data'));
+        $job_categories = JobCategory::orderBy('name', 'asc')->take(9)->get();
+
+        return view('front.home', compact('home_page_data', 'job_categories'));
     }
 }
